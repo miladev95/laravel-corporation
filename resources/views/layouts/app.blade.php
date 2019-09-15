c06cd6bd631ddce3c0fb22cd185f1699eaf0337f
milad <miladnorozii33@gmail.com>
2019-09-15 23:30:20 +0430

    admin panel added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  dir="rtl">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,19 +7,46 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'xCorporation') }}</title>
+    <title>{{ config('app.name', 'XCorporation Admin Panel') }}</title>
 
+
+    <!-- Main Theme -->
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> -->
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+
+
+    <!-- Admin Panel Links -->
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+
+
+    <!--Morris Chart CSS -->
+    <link rel="stylesheet" href="{{ asset('plugins/morris/morris.css') }}">
+
+    <!-- App css -->
+    <link href="{{ asset('css/bootstrap-rtl.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/core.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/components.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/icons.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/pages.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/menu.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="{{ asset('js/modernizr.min.js') }}"></script>
+
 </head>
-<body>
+<body class="fixed-left">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -76,5 +103,37 @@
             @yield('content')
         </main>
     </div>
+    <script>
+        var resizefunc = [];
+    </script>
+
+    <!-- jQuery  -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-rtl.min.js') }}"></script>
+    <script src="{{ asset('js/detect.js') }}"></script>
+    <script src="{{ asset('js/fastclick.js') }}"></script>
+    <script src="{{ asset('js/jquery.blockUI.js') }}"></script>
+    <script src="{{ asset('js/waves.js') }}"></script>
+    <script src="{{ asset('js/jquery.nicescroll.js') }}"></script>
+    <script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
+    <script src="{{ asset('js/jquery.scrollTo.min.js') }}"></script>
+
+    <!-- KNOB JS -->
+    <!--[if IE]>
+    <script type="text/javascript" src="assets/plugins/jquery-knob/excanvas.js"></script>
+    <![endif]-->
+
+    <script src="{{ asset('plugins/jquery-knob/jquery.knob.js') }}"></script>
+
+    <!--Morris Chart-->
+    <script src="{{ asset('plugins/morris/morris.min.js') }}"></script>
+    <script src="{{ asset('plugins/raphael/raphael-min.js') }}"></script>
+
+    <!-- Dashboard init -->
+    <script src="{{ asset('pages/jquery.dashboard.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('js/jquery.core.js') }}"></script>
+    <script src="{{ asset('js/jquery.app.js') }}"></script>
 </body>
 </html>
