b4b0341d3045a76d61890144443e80d4ef189c13
milad <miladnorozii33@gmail.com>
2020-02-04 21:45:17 +0330

    post in index page done

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -17,12 +17,12 @@ class IndexController extends Controller
     public function index()
     {
         $setting = Setting::all();
-        $slider = Slider::all();
+        $slider = Slider::all()->take(3);
         $Menu = Menu::all();
         $news = News::all()->take(3);
-        $posts = Posts::all()->take(1);
+        $post = Posts::all()->take(1)->first();
         $notification = Notification::latest('id')->first();
-        return view('index', compact('setting', 'Menu', 'slider', 'notification','news'));
+        return view('index', compact('setting', 'Menu', 'slider', 'notification','news','post'));
     }
 
     public function joinNews(NewsEmailRequest $request)
